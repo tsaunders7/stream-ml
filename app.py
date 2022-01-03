872f850eeefdaae30442e463d1dd712db5c8d8ed
tsaunders7 <97006980+tsaunders7@users.noreply.github.com>
2022-01-03 17:42:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import plot_confusion_matrix, plot_roc_curve, plot_precision_recall_curve
 from sklearn.metrics import precision_score, recall_score
+import matplotlib as plt
 
 def main():
     st.title("Binary Classification Web App")
